6ecb4d72132326b0666499ecc1784ea05458cd35
bing2008 <bing2008@126.com>
2021-12-16 23:44:33 +0800

    增加转换器命令行调用

--- test.lua
+++ test.lua
@@ -7,12 +7,41 @@ local io = require "io"
 --   out:write(s.."\r\n") --\r because windows
 --   out:flush()
 -- end
-print("It really works this time!")
+--print("It really works this time!")
 
---寰幆鎸囧畾鐩綍涓嬫墍鏈夋枃浠讹紝鍔犲叆鍒楄〃
-local srcDir = "./SrcFiles"
-for f in lfs.dir(srcDir) do
-    print(f)
+--循环指定目录下所有文件，加入列表
+local function listFile()
+    local srcDir = "./SrcFiles"
+    for f in lfs.dir(srcDir) do
+        print(f)
+    end
 end
 
---print = oldPrint
+--[[
+{
+    "InputFormat" : "UG NX 3D (*.prt)",
+    "InputFormatExt" : "UG NX 3D (*.prt)|*.prt|",
+    "OutputFormat" : "SVL2.0 (*.svlx)",
+    "OutputFormatExt" : ".svlx",
+    "ConfigFile" : "\\Converters\\SVL2.0\\CadConverter\\CadConverter.config",
+    "Input" : "UG NX 3D",
+    "Output" : "SVL2.0",
+    "ConverterInterface" : "\\Converters\\SVL2.0\\CadConverter\\CadConverter.exe"
+ },
+
+ --]]
+local function convertFile(source,target,opt)
+    --local t = io.popen('F:\\source\\SView\\转换器\\SViewConverter8.2.1.12-x64绿色版\\SViewConverterCmd.exe'.." "..source.." "..target)
+    --local t = io.popen('F:\\source\\SView\\convert\\CADExchanger\\CADExchanger.exe'.." "..source.." "..target)
+    local exe = "F:\\source\\SView\\convert\\CadConverter\\CADConverter.exe"
+    local t = io.popen(exe.." "..source.." "..target .. " " .. opt)
+    local a = t:read("*all")
+    print(a)
+    return a
+end
+
+--listFile()
+local opt = "F:\\source\\SView\\convert\\CadConverter\\CADConverter.exe"
+convertFile("SrcFiles\\pipe.prt", "TarFiles\\", opt)
+print("end")
+
